0d7ab9f73c9fa1846587c474747cf6fc8f1b1df5
Filip Grace <filip@filbot.com>
2023-04-02 17:03:08 +0000

    Update layout

--- app/src/landing.jsx
+++ app/src/landing.jsx
@@ -6,7 +6,7 @@ export default function Landing() {
   return (
     <>
       <section className="hero">
-        <h1>ERGData Dashboard</h1>
+        <h1>Logbook Dashboard</h1>
         <p>Track your rows and improve your performance with our data dashboard.</p>
         <a href="#" className="cta">Learn More</a>
       </section>
@@ -26,8 +26,25 @@ export default function Landing() {
         </div>
       </section>
 
-      <footer>
-        <p>&copy; {year} Filbot. All rights reserved.</p>
+      <footer className="bg-black rounded-lg shadow m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; {year} <a href="https://filbot.com/" className="hover:underline">Filbot™</a>. All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
       </footer>
     </>
   );
