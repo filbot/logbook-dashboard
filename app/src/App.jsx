ed173336725050768863fd624d282b9a9dff9bd5
Filip Grace <filip@filbot.com>
2023-04-02 20:39:58 +0000

    Update style and layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -12,6 +12,7 @@ import { fakeAuthProvider } from "./auth";
 import Dashboard from "./dashboard";
 import Landing from "./landing";
 import Header from "./header";
+import imgUrl from './assets/rowing-robot.jpg';
 import './App.css'
 
 export default function App() {
@@ -37,32 +38,32 @@ export default function App() {
 
 function Layout() {
   return (
-    <>
+    <div style={divStyle}>
       {/* <AuthStatus /> */}
       <Header />
       <Outlet />
-      <footer class="text-gray-400 bg-gray-900 body-font">
-        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <p class="text-sm text-gray-400 sm:py-2 sm:mt-0 mt-4">© 2020 Filbot</p>
-          <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a class="text-gray-400">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+      <footer className="text-gray-400 backdrop-blur-lg body-font">
+        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+          <p className="text-sm text-gray-400 sm:py-2 sm:mt-0 mt-4">© 2020 Filbot</p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a className="text-gray-400">
+              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-400">
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <a className="ml-3 text-gray-400">
+              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-400">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+            <a className="ml-3 text-gray-400">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-400">
-              <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+            <a className="ml-3 text-gray-400">
+              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
                 <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                 <circle cx="4" cy="4" r="2" stroke="none"></circle>
               </svg>
@@ -70,9 +71,17 @@ function Layout() {
           </span>
         </div>
       </footer>
-    </>
+    </div>
   );
 }
+
+const divStyle = {
+  backgroundImage: `url(${imgUrl})`,
+  backgroundSize: 'cover',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+};
 
 let AuthContext = React.createContext();
 
--- app/src/landing.jsx
+++ app/src/landing.jsx
@@ -1,17 +1,14 @@
-import imgUrl from './assets/react.svg';
-
 export default function Landing() {
 
   const d = new Date();
   let year = d.getFullYear();
 
   return (
-    <section class="text-gray-400 bg-gray-900 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="text-center mb-20">
-          <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">Visiualize Your Logbook Metrics</h1>
-          <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Maximize your rowing performance with Logbook-Dashboard's easy-to-use platform, analyze your data, and track your progress to crush your personal records.</p>
-          <img src={imgUrl} />
+    <section className="text-gray-400 body-font grow">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="text-center mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">Visiualize Your Logbook Metrics</h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Maximize your rowing performance with Logbook-Dashboard's easy-to-use platform, analyze your data, and track your progress to crush your personal records.</p>
         </div>
       </div>
     </section>
