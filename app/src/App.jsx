68514562e54b2f4b1f29b3f3935a90bc1bd69fd6
Filip Grace <filip@filbot.com>
2023-04-02 15:23:07 +0000

    Basic route setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { AuthProvider } from 'react-auth-kit'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello, World!!!</h1>
-    </div>
+    <AuthProvider authType={'cookie'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === "https:"}>
+      <div className="App">
+        <h1>Hello, World!!!</h1>
+      </div>
+    </AuthProvider>
+
   )
 }
 
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+import ErrorPage from "./error-page";
+import Dashboard from "./dashboard";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
