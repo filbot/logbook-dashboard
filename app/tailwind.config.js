e9fdfbcab150e6486ec15782e4052a3b4d0bf080
Filip Grace <filip@filbot.com>
2023-04-02 17:16:36 +0000

    Fix tailwind config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
