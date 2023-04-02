e3e45fc48ff960981f60a79fd58fa784e7421874
Filip Grace <filip@filbot.com>
2023-04-02 16:30:16 +0000

    Routing configuration change

--- app/src/landing.jsx
+++ app/src/landing.jsx
@@ -1,6 +1,34 @@
 export default function Landing() {
 
+  const d = new Date();
+  let year = d.getFullYear();
+
   return (
-    <h1>Landing</h1>
+    <>
+      <section className="hero">
+        <h1>ERGData Dashboard</h1>
+        <p>Track your rows and improve your performance with our data dashboard.</p>
+        <a href="#" className="cta">Learn More</a>
+      </section>
+
+      <section className="features">
+        <div className="feature">
+          <h2>Track Your Progress</h2>
+          <p>Our dashboard allows you to track your progress over time, so you can see your improvement with each row.</p>
+        </div>
+        <div className="feature">
+          <h2>Set Goals</h2>
+          <p>Set rowing goals for yourself and track your progress towards achieving them.</p>
+        </div>
+        <div className="feature">
+          <h2>Compete With Others</h2>
+          <p>Join our community and compete with other rowers to see who can row the furthest or the fastest.</p>
+        </div>
+      </section>
+
+      <footer>
+        <p>&copy; {year} Filbot. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
