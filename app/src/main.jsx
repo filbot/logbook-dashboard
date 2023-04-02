922c516557e6620a42a7ca1c10c6a5c220ad3284
Filip Grace <filip@filbot.com>
2023-04-02 15:49:48 +0000

    Configure simple auth routing

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import ErrorPage from "./error-page";
-import Dashboard from "./dashboard";
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
